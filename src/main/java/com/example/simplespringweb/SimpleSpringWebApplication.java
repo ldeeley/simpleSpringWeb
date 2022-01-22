045ff56e67a99867d0568d0454960bd9f0110255
Lester Deeley <lester.deeley@yahoo.com>
2022-01-22 16:15:36 +0000

    Initial commit

---
+++ src/main/java/com/example/simplespringweb/SimpleSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplespringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringWebApplication.class, args);
+    }
+
+}
